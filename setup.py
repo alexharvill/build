ed0222c06865312b56ee14efaab0c46b4a88cdf1
Alex Harvill <alex@vismachina.com>
2020-10-06 22:27:43 -0700

    format

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='vm_build_utils',
-      version='0.9',
-      description='vm build utility package',
-      author='Alex Harvill',
-      author_email='alex.harvill@the_service_that_sounds_like_g_mail.com',
-      url='https://https://github.com/alexharvill/build',
-      packages=['vm_build_utils'],
-     )
+setup(
+    name='vm_build_utils',
+    version='0.9',
+    description='vm build utility package',
+    author='Alex Harvill',
+    url='https://https://github.com/alexharvill/build',
+    packages=['vm_build_utils'],
+)
